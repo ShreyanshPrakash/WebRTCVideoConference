ea31dcbcee2ee1a61477e425b24e706f1e214381
Shreyansh Prakash <shreyansh@frostinteractive.com>
2020-05-25 18:21:21 +0530

    Landing page design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  Container, Grid, Paper, Button,
+} from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Container maxWidth="xs" className="container">
+        <Grid container spacing={2} justify="center">
+          <AccountCircleOutlinedIcon/>
+        </Grid>
+        <Grid container justify="space-evenly" spacing={2}>
+            <Grid item>
+              <Button variant="contained" color="primary" size="small">
+                Create Meeting
+              </Button>
+            </Grid>
+            <Grid item>
+              <Paper elevation={3} className="paper">
+                Create Meeting
+              </Paper>
+            </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/form.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FormComponent(){
+
+    return(
+        
+    )
+}
+
+export {
+    FormComponent,
+}
